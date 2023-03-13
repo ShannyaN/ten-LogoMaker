893e25bf42d3710b07df0f4ebc4ed60dd7242482
Shannya <niveyro101@gmail.com>
2023-03-12 23:50:31 -0400

    square test added

--- tests/shape.test.js
+++
@@ -1,8 +0,0 @@
-const Shape = require('../lib/shape.js');
-
-describe('Shape class',() => {
-    it('should have a text string with a max of three', () => {
-        const shape = new Shape;
-        expect(shape.text).toBe("<=3")
-    })
-} )
--- tests/square.test.js
+++ tests/square.test.js
@@ -1,8 +1,18 @@
 const Square = require('../lib/square.js');
 
 describe('Square class',() => {
-    it("should be an instance of Square", () => {
-        const newSquare = new Square(); 
-        expect(newSquare).toBeInstanceOf(Square);
+    describe('Square class', () => {
+        it("Should be an instance of Square", () => {
+            const newSquare = new Square(); 
+            expect(newSquare).toBeInstanceOf(Square);
+        })
     })
-} )
+    describe('Text Length', () => {
+        it('should throe error if text is more than 3 or less than 1', () => {
+          const newSquare = new Square(york, red, green);
+          const err = new Error('Text must be three characters or less.')
+            
+          expect(result).toEqual(false);
+        });
+      });*/
+})
