572518cc44117fc514de6145ef90b380dcec0888
Shannya <niveyro101@gmail.com>
2023-03-20 17:41:23 -0400

    color validation tests added

--- tests/square.test.js
+++ tests/square.test.js
@@ -33,19 +33,54 @@ describe('Square class',() => {
       describe('Square Class', () => {
         it('Text must be at least one character long and three characters long maximum.', () => {
           const newSquare = new Square();
-          //newSquare.text = "york";
           const err = new Error("Text must be at least one character long and three characters long maximum.");
           expect(()=>newSquare.setText("york")).toThrow(err);
       });
     });
-  //   describe('Square Class', () => {
-  //     it('Text must be at least one character long and three characters long maximum.', () => {
-  //       const newSquare = new Square();
-  //       newSquare.text = "york";
-  //       const err = new Error("Text must be at least one character long and three characters long maximum.");
-  //       expect(newSquare.text).toThrowError(err);
-  //   });
-  // });
+    describe('Color Validation', () => {
+      it('Should throw an error if the text color is not a valid color keyword or hexadecimal number.', () => {
+        const newSquare = new Square();
+        const err = new Error("Colors must be a valid color keyword or hexadecimal number.");
+        expect(()=>newSquare.colorShapeVal("red")).not.toThrow(err);
+       });
+      });
+    describe('Color Validation', () => {
+      it('Should throw an error if the text color is not a valid color keyword or hexadecimal number.', () => {
+        const newSquare = new Square();
+        const err = new Error("Colors must be a valid color keyword or hexadecimal number.");
+        expect(()=>newSquare.colorShapeVal("111111")).not.toThrow(err);
+        });
+      });
+    describe('Color Validation', () => {
+      it('Should throw an error if the text color is not a valid color keyword or hexadecimal number.', () => {
+        const newSquare = new Square();
+        const err = new Error("Colors must be a valid color keyword or hexadecimal number.");
+        expect(()=>newSquare.colorShapeVal("k")).toThrow(err);
+        });
+       });
+       describe('Color Validation', () => {
+        it('Should throw an error if the text color is not a valid color keyword or hexadecimal number', () => {
+          const newSquare = new Square();
+          const err = new Error("Colors must be a valid color keyword or hexadecimal number.");
+          expect(()=>newSquare.colorTextVal("green")).not.toThrow(err);
+         });
+        });
+      describe('Color Validation', () => {
+        it('Should throw an error if the text color is not a valid color keyword or hexadecimal number', () => {
+          const newSquare = new Square();
+          const err = new Error("Colors must be a valid color keyword or hexadecimal number.");
+          expect(()=>newSquare.colorTextVal("0000FF")).not.toThrow(err);
+          });
+        });
+      describe('Color Validation', () => {
+        it('Should throw an error if the text color is not a valid color keyword or hexadecimal number.', () => {
+          const newSquare = new Square();
+          const err = new Error("Colors must be a valid color keyword or hexadecimal number.");
+          expect(()=>newSquare.colorTextVal("0")).toThrow(err);
+          });
+         });
+       
+
       //npm run test
   })
 })
--- try.js
+++ try.js
@@ -1,6 +1,12 @@
-function hexCheck(colorCode){
+const { default: validateColor } = require("validate-color");
+var outCome;
+function hexCheck(textColor){
     var re = /[0-9A-Fa-f]{6}/g;
-    console.log(re.test(colorCode));
-    re.lastIndex = 0;
-}
-hexCheck('111111');
+    outCome = re.test(textColor);
+    console.log('Outcome is: ' + outCome)
+    return outCome;
+    re.lastIndex = 0;}
+hexCheck('k')
+console.log('validate Color outcome: ' + validateColor('k'))
+console.log('hexCheck outcome: ' +hexCheck('k'))
+console.log('Both outcome: ' + (!(validateColor('k') || hexCheck('k'))))
